bbc7d033fc0ead984aecaf150de5abf0ea3cf44e
nhan <nhan@codistry.io>
2017-06-19 22:28:24 -0700

    Added types.js

---
+++ 2_types.js
@@ -0,0 +1,25 @@
+//JavaSripty Types
+
+//Number
+
+var number = 10;
+var aFloat = 10.0;
+
+var ten = parseInt("10", 10);
+var tenFloat = parseFloat("10.0", 10);
+
+var a = + "4";
+
+//String
+
+var numberString = "ten";
+
+//Boolean
+
+var bool = false;
+
+//Objects
+
+//Null
+
+//Undefined
